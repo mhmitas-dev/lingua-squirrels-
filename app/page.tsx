a9fe56159137f78f10efa5fd3b1e37d1309e41f6
mhmitas-dev <mhmitas.dev@gmail.com>
2025-12-09 09:01:13 +0600

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@/components/ui/button';
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen ">
+     <Button variant={"destructive"}>Test Button</Button>
+    </div>
+  );
+}
